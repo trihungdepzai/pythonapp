de8dc33e317cd3c8c29db69503db4eb2b344ba86
trihungdepzai <jason.hou1006@gmail.com>
2025-03-09 17:18:52 +0700

    helooooo

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import sqlite3
 
 def dict_factory(cursor, row):
     d = {}
-    for idx, col, in enumerate(cursor.description):
+    for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
-        return d
+    return d
 
 def connect_db():
     conn = sqlite3.connect('data/database.db')
@@ -41,5 +41,3 @@ def get_user_by_email_and_password(email, password):
     user = cursor.fetchone()
     conn.close()
     return user
-
-print(get_user_by_id(1))
